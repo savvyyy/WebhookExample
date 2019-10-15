4514556222ee9bf500a3e752dc0bafe5db59bc71
itadmin <itadmin@lapr101.successivetech.com>
2019-10-15 18:54:14 +0530

    new one

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3001;
 
 app.get('/', function(req, res) {
     res.send('app.js is running!!');
+    console.log('hi')
 })
 
 app.listen(port , () => console.log(`Server is listening on the port ${port}`))
